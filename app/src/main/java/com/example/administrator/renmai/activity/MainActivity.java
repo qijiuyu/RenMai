69d3c53b26d112abd25d2a2793ff686dcbff9110
jiuyu.qi@edaibu.net <qijiuyu79>
2019-01-13 14:22:45 +0800

    接入easeui

--- app/src/main/java/com/example/administrator/renmai/activity/MainActivity.java
+++ app/src/main/java/com/example/administrator/renmai/activity/MainActivity.java
@@ -1,12 +1,45 @@
 package com.example.administrator.renmai.activity;
 
+import android.Manifest;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.example.administrator.renmai.utils.PermissionsUtils;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //两个日历权限和一个数据读写权限
+        String[] permissions = new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE};
+//        PermissionsUtils.showSystemSetting = false;//是否支持显示系统设置权限设置窗口跳转
+        //这里的this不是上下文，是Activity对象！
+        PermissionsUtils.getInstance().chekPermissions(this, permissions, permissionsResult);
+    }
+
+
+    //创建监听权限的接口对象
+    PermissionsUtils.IPermissionsResult permissionsResult = new PermissionsUtils.IPermissionsResult() {
+        @Override
+        public void passPermissons() {
+            Toast.makeText(MainActivity.this, "权限通过，可以做其他事情!", Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void forbitPermissons() {
+//            finish();
+            Toast.makeText(MainActivity.this, "权限不通过!", Toast.LENGTH_SHORT).show();
+        }
+    };
+
+
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        //就多一个参数this
+        PermissionsUtils.getInstance().onRequestPermissionsResult(this, requestCode, permissions, grantResults);
     }
 }
